f6b58cb90d5759b57ac0c0ebe100d40e977a9cbb
Tushar Gangurde <tushargangurde859@gmail.com>
2025-07-25 11:36:00 +0530

    Update A.js

--- A.js
+++ A.js
@@ -1,16 +1,21 @@
-SELECT 
-    si.number, 
-    si."short_description", 
-    si."assigned_to.name" AS uid, 
+SELECT
+    si.number,
+    si."short_description",
+    si."assigned_to.name" AS username,
+    si."assigned_to.user_name" AS Uid,
+    ac.name AS clusterName,
+    as2.name AS subcluster,
+    ' ' AS "Date",
+    '00' AS "Time",
     CONCAT(
         FLOOR(COALESCE((
-            SELECT SUM(ael.hours_spent) 
-            FROM pilothouse.aps_efforts_logging ael 
+            SELECT SUM(ael.hours_spent)
+            FROM pilothouse.aps_efforts_logging ael
             WHERE ael.task_id = si.number
         ), 0) / 60)::TEXT, 'hr ',
         MOD(COALESCE((
-            SELECT SUM(ael.hours_spent) 
-            FROM pilothouse.aps_efforts_logging ael 
+            SELECT SUM(ael.hours_spent)
+            FROM pilothouse.aps_efforts_logging ael
             WHERE ael.task_id = si.number
         ), 0), 60)::TEXT, 'min'
     ) AS "Total Hours"
@@ -21,7 +26,7 @@ FULL JOIN pilothouse.apm_application_daps aad ON sba.goldenapp_auid = aad.golden
 FULL JOIN pilothouse.apm_applicatons aa ON aa.id = aad.apm_application
 FULL JOIN pilothouse.apm_clusters ac ON aa.apm_cluster = ac.id
 FULL JOIN pilothouse.apm_subclusters as2 ON aa.apm_subcluster = as2.id
-WHERE LOWER('$type') = 'incident' 
+WHERE LOWER('$type') = 'incident'
   AND si."number" LIKE 'INC%'
   AND ac.id IN ($ApmCluster)
   AND as2.id IN ($ApmSubCluster)
